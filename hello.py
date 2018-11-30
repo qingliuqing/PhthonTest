ecfb55592cb4c7b111e8e2d70871ced90cca1d65
liuqing <qing_liu@bestsign.cn>
2018-11-30 16:06:34 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,19 @@
+# name = input('please enter your name')
+# print('hello,', name)
+
+# Python使用缩进来组织代码块，请务必遵守约定俗成的习惯，坚持使用4个空格的缩进。
+# 在文本编辑器中，需要设置把Tab自动转换为4个空格，确保不混用Tab和空格。
+# a = 100
+# if a >= 0:
+# 	print(a)
+# else: 
+# 	print(-a)
+	
+# print('''000
+# 111
+# 222
+# 333''')
+# 
+
+# list
+# tuple
